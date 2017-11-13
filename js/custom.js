ef84fbe5b2594cba1cafff960c246bbe452dd02c
José Oliveira <josecarlosdeoliveirajr@gmail.com>
2017-11-13 08:15:53 -0200

    Fixed error on scroll navbar.

--- js/custom.js
+++ js/custom.js
@@ -10,14 +10,14 @@
       $('.navbar-nav li').removeClass('active');
       $(this).parent().addClass('active');
     }
-    
+
     if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html, body').animate({
           scrollTop: target.offset().top
-          
+
         }, 1000, function() {
           clickState = 1;
         });
